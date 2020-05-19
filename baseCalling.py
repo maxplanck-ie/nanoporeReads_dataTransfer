e6c2663863b8cef5f214c88fc7125858af8f14fe
Pipeline Project User <pipegrp@rapidus.ie-freiburg.mpg.de>
2020-05-19 13:20:28 +0200

    updated the pipeline on a case with barecodes

--- baseCalling.py
+++ baseCalling.py
@@ -3,30 +3,29 @@ import sys
 import os
 import subprocess as sp
 
-def base_calling(config):
+def base_calling(config,bc_kit):
     """
     run guppy basecaller
     """
     cmd = config["guppy_basecaller"]["base_calling_cmd"]
     fast5 = config["info_dict"]["fast5"]
-    os.mkdir(config["info_dict"]["flowcell_path"]+"/Project_"+config["data"]["Sample_Project"])
-    fastq = os.path.join(config["info_dict"]["flowcell_path"],"Project_"+config["data"]["Sample_Project"])
-    os.mkdir(fastq+"/Sample_"+config["data"]["Sample_Name"])
-    fastq = os.path.join(fastq,"Sample_"+config["data"]["Sample_Name"])
-    config["info_dict"]["fastq"] = fastq
+    os.mkdir(config["info_dict"]["flowcell_path"]+"/fastq")    
+    fastq = os.path.join(config["info_dict"]["flowcell_path"],"fastq")
     flowcell=config["info_dict"]["flowcell"]
     barcode = False
-    if config["data"]["barcode_kits"] is not "no_bc": #At the moment there is no multi samples per run so i dunno how would fast5 look and how i should update the code for that
+    if bc_kit is not "no_bc":
         barcode = True
     kit=config["info_dict"]["kit"]
     cmd += " -i {} -s {} --flowcell {} --kit {} ".format(fast5,fastq,flowcell,kit)
     cmd += config["guppy_basecaller"]["base_calling_options"]
     if barcode is True:
-        cmd += " --barcode_kits {} ".format(kit)
+        cmd += " --barcode_kits {} ".format(bc_kit)
         cmd += config["guppy_basecaller"]["base_calling_barcode_options"]
     else:
         cmd += " -q 0 " # TODO test on sampels with barcodes
     # RNA
     if "RNA" in config["info_dict"]["kit"]:
         cmd += " "+config["guppy_basecaller"]["base_calling_RNA_options"]
+   
+    print(cmd) 
     sp.check_call(cmd, shell=True)
