04dc55dc189306afa807745791bf975df03dc214
Leily Rabbani <rabbani@pc390.ie-freiburg.mpg.de>
2020-10-08 13:47:22 +0200

    added bam qc

--- qc.py
+++ qc.py
@@ -19,20 +19,18 @@ def fastq_qc(config,data):
         sp.check_call(cmd, shell=True)
     return None
 
-def pycoQc(config, data):
+def pycoQc_fastq(config, data, bc_kit):
     wd = os.path.join(config["info_dict"]["flowcell_path"])
-    cmd = config["pycoQc"]["barcodeSplit"]
-    cmd += wd+"/fastq/sequencing_summary.txt "
-    cmd += "-o "+wd
-    print(cmd)
-    QC_path = ""
-    sp.check_call(cmd, shell=True)
-    for k, v in data.items():
-        fastqc = os.path.join(wd+"/FASTQC_Project_"+v["Sample_Project"])
-        if not os.path.exists(wd+"/FASTQC_Project_"+v["Sample_Project"]):
-           os.mkdir(wd+"/FASTQC_Project_"+v["Sample_Project"])
-        QC_path = fastqc
-        if v["barcode_kits"] is not "no_bc":
+    if bc_kit is not "no_bc":
+        cmd = config["pycoQc"]["barcodeSplit"]
+        cmd += wd+"/fastq/sequencing_summary.txt "
+        cmd += "-o "+wd
+        print(cmd)
+        sp.check_call(cmd, shell=True)
+        for k, v in data.items():
+            fastqc = os.path.join(wd+"/FASTQC_Project_"+v["Sample_Project"])
+            if not os.path.exists(wd+"/FASTQC_Project_"+v["Sample_Project"]):
+                os.mkdir(wd+"/FASTQC_Project_"+v["Sample_Project"])
             barcode = v["index_id"]
             os.mkdir(fastqc+"/Sample_"+v["Sample_ID"])
             fastqc = os.path.join(fastqc+"/Sample_"+v["Sample_ID"])
@@ -42,8 +40,39 @@ def pycoQc(config, data):
             print(cmd)
             sp.check_call(cmd, shell=True)
 
-    cmd = config["pycoQc"]["pycoQc"]+" "
-    cmd += wd+"/fastq/sequencing_summary.txt "
-    cmd += "-o "+QC_path+"/pycoqc.html"
-    print(cmd)
-    sp.check_call(cmd, shell=True)
+        cmd = config["pycoQc"]["pycoQc"]+" "
+        cmd += wd+"/fastq/sequencing_summary.txt "
+        cmd += "-o "+wd+"/pycoqc.html"
+        print(cmd)
+        sp.check_call(cmd, shell=True)
+    else:
+        assert(len(data.items()) == 1)
+        project_name = list(data.values())[0]["Sample_Project"]
+        fastqc = os.path.join(wd+"/FASTQC_Project_"+project_name)
+        print(fastqc)
+        assert(os.path.exists(wd+"/FASTQC_Project_"+project_name) == 0)
+        os.mkdir(wd+"/FASTQC_Project_"+project_name)
+        cmd = config["pycoQc"]["pycoQc"]+" "
+        cmd += wd+"/fastq/sequencing_summary.txt "
+        cmd += "-o "+fastqc+"/pycoqc.html"
+        print(cmd)
+        sp.check_call(cmd, shell=True)
+
+
+def pycoQc_bam(config, data, bc_kit, ref):
+    wd = os.path.join(config["info_dict"]["flowcell_path"])
+    if bc_kit  == 'no_bc':
+        assert(len(data.items()) == 1)
+        sample_project = list(data.values())[0]["Sample_Project"]
+        sample_id = list(data.values())[0]["Sample_ID"]
+        bam = list(data.values())[0]["Sample_Name"]+".bam"
+        group=sample_project.split("_")[-1].lower()
+        final_path = config["paths"]["groupDir"]+group+"/sequencing_data/OxfordNanopore/"+config["input"]["name"]
+        path_to_bam = final_path+"/Analysis_"+sample_project+"/mapping_on_"+ref+"/"+sample_id
+        bam = os.path.join(path_to_bam, bam)
+        cmd = config["pycoQc"]["pycoQc"]+" "
+        cmd += wd+"/fastq/sequencing_summary.txt "
+        cmd += " -a "+bam
+        cmd += " -o "+path_to_bam+"/pycoqc_bam.html"
+        print(cmd)
+        sp.check_call(cmd, shell=True)
