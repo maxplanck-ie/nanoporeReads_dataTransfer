6f2bfc0b003156f5d6a1d2858b813b5c4e60e218
adRn-s <salatino@omics.dev>
2024-08-19 14:54:21 +0200

    IMPORTANT: adjust config-parkour-url before running this version. api endpoints are hardcoded

--- src/npr/ont_pipeline.py
+++ src/npr/ont_pipeline.py
@@ -116,12 +116,11 @@ def find_new_flowcell(config):
         )
 
         if not os.path.isfile(ss):
-            # here we need some method to retrieve the SampleSheet.csv from Parkour
-            # get_samplesheet_from_parkour(flowcell, config)
-            if not os.path.isfile(ss2):
-                msg = "No SampleSheet.csv file.\n"
-                send_email("Error for flowcell:", msg, config)
-                sys.exit("no sampleSheet.")
+            if not get_samplesheet_from_parkour(flowcell, config, ss2):
+                if not os.path.isfile(ss2):
+                    msg = "No SampleSheet.csv file.\n"
+                    send_email("Error for flowcell:", msg, config)
+                    sys.exit("no sampleSheet.")
 
         # return flowcell to ont()
         msg = "SampleSheet.csv file found.\n"
@@ -130,6 +129,48 @@ def find_new_flowcell(config):
         return (os.path.basename(flowcell), msg, flowcell)
 
     return (None, None, None)
+
+
+def get_samplesheet_from_parkour(flowcell, config, output_csv_path):
+    """
+    Saves the samplesheet from parkour, directly to the outputDir
+    """
+    postfixes = ["-5", "-4", "-3", "-2", "-1", ""]
+
+    for pf in postfixes:
+        flowcell_id = flowcell + pf
+        params = {"flowcell_id": flowcell_id}
+
+        try:
+            res = requests.get(
+                config["parkour"]["url"] + "/api/flowcells/retrieve_samplesheet/",
+                auth=(config["parkour"]["user"], config["parkour"]["password"]),
+                params=params,
+                verify=config["parkour"]["pem"],
+            )
+            if res.status_code == 200:
+                with open("/tmp/output_file.xls", "wb") as file:
+                    file.write(res.content)
+                print(
+                    f"Samplesheet for flowcell {flowcell_id} saved successfully under /tmp."
+                )
+                df = pd.read_excel("/tmp/output_file.xls")
+                df.to_csv(output_csv_path, index=False)
+                print(
+                    f"Samplesheet for flowcell {flowcell_id} available at {output_csv_path}"
+                )
+
+                return True
+
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed for flowcell {flowcell_id}: {e}")
+
+        except IOError as e:
+            print(f"File operation failed for flowcell {flowcell_id}: {e}")
+
+    # Raise an error if none of the requests were successful
+    # raise Exception("Failed to retrieve samplesheet for any flowcell ID.")
+    return False
 
 
 def read_flowcell_info(config, info_dict, base_path):
