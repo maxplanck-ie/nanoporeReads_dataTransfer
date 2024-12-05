5d0a4948c476a7579f7b9ef22be6767423cd2c0c
NavandarM <navandar@ie-freiburg.mpg.de>
2024-12-05 16:40:08 +0100

    Deprecated 'get_organism_name' function and changed the way to get organism name

--- src/npr/communication.py
+++ src/npr/communication.py
@@ -222,9 +222,10 @@ def query_parkour(config, flowcell, msg):
             print(parkour_dict)
             first_key = list(parkour_dict.keys())[0]
             first_entry = list(parkour_dict[first_key].keys())[0]
-            org = parkour_dict[first_key][first_entry][-3]
-            organism = get_organism_name(org)
-            # organism = parkour_dict[first_key][first_entry][-3]
+            org = parkour_dict[first_key][first_entry][3][0]
+            organism = org
+            print (org)
+            #organism = parkour_dict[first_key][first_entry][-3]
             protocol = parkour_dict[first_key][first_entry][1]
 
             if (
@@ -261,15 +262,3 @@ def query_parkour(config, flowcell, msg):
     send_email("Error with flowcell:", msg, config)
     sys.exit("parkour failure.")
 
-
-def get_organism_name(organism):
-    if "GRCm38" in organism:
-        return "GRCm38"
-    elif "GRCm39" in organism:
-        return "GRCm39"
-    elif "GRCh37" in organism:
-        return "GRCh37"
-    elif "GRCh38" in organism:
-        return "GRCh38"
-    else:
-        return organism
