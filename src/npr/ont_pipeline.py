f983c71130512cb15803acdb2d2cb00a349a2d89
Pipeline Project User <pipegrp@deep21.ie-freiburg.mpg.de>
2025-06-13 11:29:23 +0200

    extracting model from modified json file

--- src/npr/ont_pipeline.py
+++ src/npr/ont_pipeline.py
@@ -276,6 +276,15 @@ def read_flowcell_info(config, info_dict, base_path):
                 print(
                     f"  [green]Found alignment is already done. Assuming sorted bam.[/green]"
                 )
+            else:
+                for arg in args:
+                    match = re.match(r'simplex_model="?([^"]+)"?', arg)
+                    if match:
+                        print("Simplex model extracted!")
+                        model = match.group(1)
+                        info_dict["model_def"] = model
+                        info_dict["model"] = model
+
 
         if not model:
             print(
