73dce89fd83bc0aaadf03b1124ef11a04a294d3f
Galina <galina.erikson@gmail.com>
2025-07-16 13:54:19 +0200

    chaning path to TLS CA certificate bundle

--- src/npr/communication.py
+++ src/npr/communication.py
@@ -206,11 +206,22 @@ def query_parkour(config, flowcell, msg):
     for pf in postfixes:
         d = {"flowcell_id": fc + pf}
         flowcellqueries.append(fc + pf)
+        cert="/home/pipegrp/cert-parkour2-dev.pem"
+        #res = requests.get(
+        #    config["parkour"]["url"] + "/api/analysis_list/analysis_list/",
+        #    auth=(config["parkour"]["user"], config["parkour"]["password"]),
+        #    params=d,
+        #    verify=config["parkour"]["pem"],
+        #)
+
         res = requests.get(
-            config["parkour"]["url"] + "/api/analysis_list/analysis_list/",
-            auth=(config["parkour"]["user"], config["parkour"]["password"]),
-            params=d,
-            verify=config["parkour"]["pem"],
+        config['parkour']['URL'] + '/api/analysis_list/analysis_list/',
+        auth=(
+            config['parkour']['user'],
+            config['parkour']['password']
+        ),
+        params=d,
+        verify=config['parkour']['cert']
         )
         if res.status_code == 200:
             info_dict = {}
